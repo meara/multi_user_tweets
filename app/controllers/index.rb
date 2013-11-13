dc570ebe4cff0284197873e376934b4e694a8072
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-12 18:29:30 -0600

    first commit

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -20,6 +20,25 @@ get '/auth' do
 
   # at this point in the code is where you'll need to create your user account and store the access token
 
-  erb :index
+  # @access_token.inspect
+  # @access_token.params[:screen_name]
+  user = User.create(username: @access_token.params[:screen_name], oauth_token: @access_token.token, oauth_secret: @access_token.secret)
+  session[:user_id]=user.id
   
+  redirect '/'
+end
+
+
+post '/tweet' do 
+
+  begin
+    user = User.find_by_id(session[:user_id])
+    person = Twitter::Client.new( oauth_token: user.oauth_token, oauth_token_secret: user.oauth_secret)
+    person.update(params[:tweet])
+  rescue
+    "Error.  No tweets for you. :("    
+  else
+    "Tweet submitted!"
+  end
+
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  
+  $('#tweet-form').on('submit', function(event) {
+    event.preventDefault();
+    var data= $(this).serialize();
+    // console.log(data);
+    $("#tweet-form").prop('disabled', true)
+    if ($('#response')) {
+      $('#response').remove();
+    }
+    $('.messages').append("<p class='progress'>Sending tweet...</p>")
+    $.post('/tweet', data, function(response) {
+      console.log(response);
+      $('.messages').append("<p id='response'>" + response + "</p>");
+      $('.progress').remove();
+      $("#tweet-form").prop('disabled', false);
+      $("#tweet-text").prop('value', '');
+    });
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  });
+
 });
